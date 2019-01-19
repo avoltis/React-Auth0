6844e6edd678feab7d8072ff077e45811b5bab66
Voltis <v.agolli@live.com>
2019-01-19 11:38:11 +0100

    API server calls

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Profile from './Profile';
 import Nav from './Nav';
 import Auth from './Auth/Auth';
 import Callback from './Callback';
+import Public from './Public';
 
 class App extends Component {
   constructor(props) {
@@ -34,6 +35,7 @@ class App extends Component {
                 : <Redirect to="/" />
             }
           />
+          <Route path="/public" component={Public}/>
         </div>
       </React.Fragment>
     );
--- src/Nav.js
+++ src/Nav.js
@@ -10,6 +10,7 @@ class Nav extends Component {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/profile">Profile</Link></li>
+                    <li><Link to="/public">Public</Link></li>
                     <li>
                         <button onClick={isAuthenticated() ? logout : login}>
                              {isAuthenticated() ? 'Log Out' : 'Log In'}
